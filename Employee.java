31f0352313df278b9b3ff6504c13c66e7c952308
akshat645 <31894995+akshat645@users.noreply.github.com>
2019-01-18 17:11:25 +0530

    POJO class Added
    The main Employee .java file which have all the setter and getter methods

---
+++ Employee.java
@@ -0,0 +1,59 @@
+package POJO;
+public class Employee {
+private int eid;
+private String ename;
+private String dept;
+private int salary; 
+private long ph_no;
+private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public long getPh_no() {
+        return ph_no;
+    }
+
+    public void setPh_no(long ph_no) {
+        this.ph_no = ph_no;
+    }
+
+}
